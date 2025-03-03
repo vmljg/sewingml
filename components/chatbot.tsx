1b896846600853e772abb6dc871d1708cde188f1
vmljg <jason.gardner@vml.com>
2025-03-03 12:50:04 -0600

    upd: Fix build/lint step

--- components/chatbot.tsx
+++ components/chatbot.tsx
@@ -1,53 +1,61 @@
-'use client';
+"use client";
 
-import { useChat } from '@ai-sdk/react';
+import { useChat } from "@ai-sdk/react";
 
 function Spinner() {
   return <div>Loading...</div>;
 }
 
 export default function Chatbot() {
-  const { messages, input, handleInputChange, handleSubmit, status, stop, reload, error , data} =
-    useChat({
-        streamProtocol: "text"
-    });
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    status,
+    stop,
+    reload,
+    error,
+    data,
+  } = useChat({
+    streamProtocol: "text",
+  });
 
   return (
     <>
-    {data && <div>{data}</div>}
-      {messages.map(message => (
-  <div key={message.id}>
-    {message.role === 'user' ? 'User: ' : 'AI: '}
-    {message.parts.map((part, index) => {
-      // text parts:
-      if (part.type === 'text') {
-        return <div key={index}>{part.text}</div>;
-      }
+      {messages.map((message) => (
+        <div key={message.id}>
+          {message.role === "user" ? "User: " : "AI: "}
+          {message.parts.map((part, index) => {
+            // text parts:
+            if (part.type === "text") {
+              return <div key={index}>{part.text}</div>;
+            }
 
-      // reasoning parts:
-      if (part.type === 'reasoning') {
-        return (
-          <pre key={index}>
-            {part.details.map(detail =>
-              detail.type === 'text' ? detail.text : '<redacted>',
-            )}
-          </pre>
-        );
-      }
-    })}
-  </div>
-))}
+            // reasoning parts:
+            if (part.type === "reasoning") {
+              return (
+                <pre key={index}>
+                  {part.details.map((detail) =>
+                    detail.type === "text" ? detail.text : "<redacted>"
+                  )}
+                </pre>
+              );
+            }
+          })}
+        </div>
+      ))}
 
-      {(status === 'submitted' || status === 'streaming') && (
+      {(status === "submitted" || status === "streaming") && (
         <div>
-          {status === 'submitted' && <Spinner />}
+          {status === "submitted" && <Spinner />}
           <button type="button" onClick={() => stop()}>
             Stop
           </button>
         </div>
       )}
 
-      {status === 'error' && (
+      {status === "error" && (
         <div>
           <div>Error: {error?.message}</div>
           <button type="button" onClick={() => reload()}>
@@ -61,7 +69,7 @@ export default function Chatbot() {
           name="prompt"
           value={input}
           onChange={handleInputChange}
-          disabled={status !== 'ready'}
+          disabled={status !== "ready"}
         />
         <button type="submit">Submit</button>
       </form>
